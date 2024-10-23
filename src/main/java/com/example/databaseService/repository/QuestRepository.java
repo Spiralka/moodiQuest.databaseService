b6baea4ea949f4a49c4c0629a06f766a2671cb2d
smetanka <spiralcka@gmail.com>
2024-10-23 16:05:22 +0300

    fix DB name

--- src/main/java/com/example/databaseService/repository/QuestRepository.java
+++ src/main/java/com/example/databaseService/repository/QuestRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 @Repository
 public interface QuestRepository extends JpaRepository<Quest, Long> {
 
-    @Query(value = "SELECT * FROM quests order by RANDOM() LIMIT 3", nativeQuery = true)
+    @Query(value = "SELECT * FROM quests.public.quests order by RANDOM() LIMIT 3", nativeQuery = true)
     List<Quest> findRandomDailyQuest();
 
-    @Query(value = "SELECT * from quests order by RANDOM() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * from quests.public.quests order by RANDOM() LIMIT 1", nativeQuery = true)
     Quest findRandomQuest();
 }
